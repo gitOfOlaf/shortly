8675cddbae20e2b919cd289e9d9188c5a82562d8
olaf <olafer25227@gmail.com>
2023-03-05 08:29:45 +0100

    implemented mobile menu

--- dist/index.7ff0ec52.js
+++ dist/index.7ff0ec52.js
@@ -558,7 +558,21 @@ function hmrAccept(bundle, id) {
 
 },{}],"dUn2x":[function(require,module,exports) {
 class Generator {
-    constructor(){}
+    constructor(){
+        this.burger = document.querySelector(".burger");
+        this.form = document.querySelector("form");
+        this.mobileMenu = document.querySelector(".mobile-menu");
+        this.burger.addEventListener("click", this._toggleMenuBar.bind(this));
+        this.form.addEventListener("submit", this._submitForm.bind(this));
+    }
+    _toggleMenuBar() {
+        this.burger.classList.toggle("toggle");
+        this.mobileMenu.classList.toggle("translate-x-[120%]");
+    }
+    _submitForm(e) {
+        e.preventDefault();
+        console.log("submitted");
+    }
 }
 const gen = new Generator();
 
